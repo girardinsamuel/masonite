b1e7b4788f5314c57358b93566e1dc8f76f8ca58
Samuel Girardin <samuelgirardin@pm.me>
2022-02-08 11:28:08 +0100

    fix scaffolded auth in test project

--- tests/integrations/controllers/auth/LoginController.py
+++ tests/integrations/controllers/auth/LoginController.py
@@ -9,11 +9,17 @@ class LoginController(Controller):
     def show(self, view: View):
         return view.render("auth.login")
 
-    def store(self, view: View, request: Request, auth: Auth, response: Response):
+    def store(self, request: Request, auth: Auth, response: Response):
         login = auth.attempt(request.input("username"), request.input("password"))
 
         if login:
-            return response.redirect(name="home")
+            return response.redirect(name="auth.home")
 
         # Go back to login page
+        return response.redirect(name="login").with_errors(
+            ["The email or password is incorrect"]
+        )
+
+    def logout(self, auth: Auth, response: Response):
+        auth.logout()
         return response.redirect(name="login")
