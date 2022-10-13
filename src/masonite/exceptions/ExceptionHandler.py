ed1133fbc476d5c5963af3dc7b41388679ba9b0a
Samuel Girardin <samuelgirardin@pm.me>
2022-10-13 17:12:35 +0200

    remove duplicated code

--- src/masonite/exceptions/ExceptionHandler.py
+++ src/masonite/exceptions/ExceptionHandler.py
@@ -41,11 +41,6 @@ class ExceptionHandler:
             response.with_headers(headers)
 
         # if an exception handler is registered for this exception, use it instead
-        # add headers to response if any
-        if hasattr(exception, "get_headers"):
-            headers = exception.get_headers()
-            response.with_headers(headers)
-
         if self.application.has(f"{exception.__class__.__name__}Handler"):
             return self.application.make(
                 f"{exception.__class__.__name__}Handler"
